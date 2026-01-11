406ca123ead2c905ed6a5caeb01f2bbc19c421f7
chollasit-dev <118034127+chollasit-dev@users.noreply.github.com>
2026-01-12 01:38:40 +0700

    Set always set shell and related even prior actions failed

--- scripts/setup.bash
+++ scripts/setup.bash
@@ -7,8 +7,10 @@ set_up_login_shell() {
   # FIXME: This echo command won't execute.
   echo "deb [trusted=yes] https://apt.fury.io/rsteube/ /" | sudo tee /etc/apt/sources.list.d/fury.list &&
     sudo apt update &&
-    sudo nala install -y carapace zsh zsh-syntax-highlighting &&
-    sudo chsh -s "$(which zsh)" &&
+    sudo nala install -y zsh zsh-syntax-highlighting &&
+    sudo nala install carapace # Prevent block or skip the rest when this failed
+
+  command -v zsh >/dev/null 2>&1 && chsh -s "/usr/bin/zsh" &&
     wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh | sh # https://github.com/ohmyzsh/ohmyzsh
 }
 
